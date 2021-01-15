a19f0734ec0bb3ef981dcf3011d351ff04e10f04
Fareez <fareez.mohamad@hexainitiative.com>
2021-01-15 19:16:23 +0800

    Add query creation (findByGroupID)

--- src/main/java/com/wolf/noteScout/controller/NoteController.java
+++ src/main/java/com/wolf/noteScout/controller/NoteController.java
@@ -3,10 +3,8 @@ package com.wolf.noteScout.controller;
 import com.wolf.noteScout.model.Note;
 import com.wolf.noteScout.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -18,8 +16,37 @@ public class NoteController {
     @GetMapping("/scoutnote")
     public List<Note> index() {return noteRepository.findAll();}
 
-    @PostMapping("/scoutnote/")
+    @GetMapping("/scoutnote/{groupID}")
+    public List<Note> getNoteGroupID(@PathVariable int groupID) {
+        return noteRepository.findByGroupID(groupID);
+    }
+
+    @PutMapping("/scoutnote/save")
     Note note(@RequestBody Note note){
         return noteRepository.save(note);
+    }
+
+    @PutMapping("/scoutnote/isChecked/{id}")
+    public Note note(@PathVariable int id, @RequestBody boolean checked){
+
+        Note item = noteRepository.findOne(id);
+        int validCheck = 0;
+
+        if(item != null){
+            if(checked){
+                validCheck = 1;
+                item.setIsChecked(validCheck);
+            }else{
+                item.setIsChecked(validCheck);
+            }
+            return noteRepository.save(item);
+        }else{
+            return null;
+        }
+    }
+
+    @DeleteMapping("/scoutnote/{id}")
+    public void deleteNote(@PathVariable int id){
+        noteRepository.delete(id);
     }
 }
