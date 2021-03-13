05c86dba748915f49595735d5b12c62d65d80512
Fareez <fareez.mohamad@hexainitiative.com>
2021-03-13 12:08:09 +0800

    Added checkGroupID Endpoint

--- src/main/java/com/wolf/noteScout/controller/GroupController.java
+++ src/main/java/com/wolf/noteScout/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.wolf.noteScout.controller;
 
 import com.wolf.noteScout.model.Group;
+import com.wolf.noteScout.model.Note;
 import com.wolf.noteScout.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,28 +14,29 @@ public class GroupController {
     @Autowired
     GroupRepository groupRepository;
 
-//    @PutMapping("/scoutnote/addNewGroup")
-//    Group group(@RequestBody Group group) {
-//        return groupRepository.save(group);
-//    }
+    @GetMapping("/scoutnote/checkGroup/{groupID}")
+    public List<Group> checkGroupId(@PathVariable int groupID) {
+        return groupRepository.findByGroupId(groupID);
+    }
 
-    @PutMapping("/scoutnote/checkGroup/{groupId}/{groupOwner}")
-    public Group group(@PathVariable int groupId, @PathVariable String groupOwner) {
+    @PutMapping("/scoutnote/addGroup/{groupId}/{groupOwner}")
+    public Group addNewGroup(@PathVariable int groupId, @PathVariable String groupOwner) {
+//        Group group = new Group(
+//                groupId,
+//                groupOwner
+//        );
+//
+//        return groupRepository.save(group);
+
+        List<Group> result = groupRepository.findByGroupId(groupId);
         Group group = new Group(
                 groupId,
                 groupOwner
         );
-
-        return groupRepository.save(group);
-
-//        List<Group> result = groupRepository.findByGroupId(groupId);
-//        Group group = new Group();
-//        group.setGroupId(groupId);
-//        group.setGroupOwner(groupOwner);
-//        if(result == null) {
-//            return groupRepository.save(group);
-//        }else {
-//           return null;
-//        }
+        if(result == null) {
+            return groupRepository.save(group);
+        }else {
+           return null;
+        }
     }
 }
